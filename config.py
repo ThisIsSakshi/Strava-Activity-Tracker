029aef4f27f2ec1a3a2b82dbaa9761b0cd25b8ff
Sakshi Sharma <thisissakshisharma@gmail.com>
2025-08-20 11:20:18 +0530

    adding update_readme and config to test

---
+++ config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
+
+CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
+REFRESH_TOKEN = os.getenv("STRAVA_REFRESH_TOKEN")
